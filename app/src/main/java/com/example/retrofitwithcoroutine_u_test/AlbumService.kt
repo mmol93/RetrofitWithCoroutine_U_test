88d2fb2fd71da086faa9f30764df75bd3613c22f
Ljw <ljws93@naver.com>
2021-08-23 17:38:23 +0900

    How to use @Path, @Query annotation with Retrofit
    1. @Path
    - it's same with / in web address
    
    2. @Query
    - it's same with ? in web address
    
    3. usage of @Path & @Query is same

--- app/src/main/java/com/example/retrofitwithcoroutine_u_test/AlbumService.kt
+++ app/src/main/java/com/example/retrofitwithcoroutine_u_test/AlbumService.kt
@@ -2,6 +2,8 @@ package com.example.retrofitwithcoroutine_u_test
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface AlbumService {
     // Retrofit과 코루틴을 이용하여 값을 얻기 위해 suspend 사용
@@ -10,4 +12,21 @@ interface AlbumService {
     // 그래서 Kotlin에서 읽을 수 있음
     @GET("/albums")
     suspend fun getAlbums() : Response<Albums>
+
+    // userId가 3인 앨범들만 가져온다
+    // 아래 문구는 실제로 이렇게 작동한다 -> https://jsonplaceholder.typicode.com/albums?userId=3
+    // userId에서 받은 값 = 3 / @Query("userId") = /albums?userID
+    // 즉 End Point는 albums?userId=3 이 된다  => userId가 3인 데이터 묶음만 찾을 수 있다
+    @GET("/albums")
+    suspend fun getSortedAlbums(@Query("userId")userId:Int) : Response<Albums>
+
+    // id가 3인 앨범을 가져온다
+    // 예: https://jsonplaceholder.typicode.com/albums/3
+    @GET("/albums/{id}")
+    // Response<Albums> 이 아니라 Response<AlbumsItem>인 이유
+    // : @Query의 경우 Sort로써 정렬된 값"들"이 나온다 그래서 값들의 List인 Albums를 사용
+    // : @Path의 경우 해당 Source(여기선 id)만 가져오기 때문에 1개의 AlbumItem 형태를 띄는 행렬 1개만 가져온다
+    // -> @Query와 @Path의 경우 return 하는 값의 type 자체가 다르다
+    // Query의 경우 주소상에서 ?로 표기되고, Path의 경우 /로 표기된다
+    suspend fun getAlbum(@Path("id") albumId:Int) : Response<AlbumsItem>
 }
--- app/src/main/java/com/example/retrofitwithcoroutine_u_test/MainActivity.kt
+++ app/src/main/java/com/example/retrofitwithcoroutine_u_test/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.retrofitwithcoroutine_u_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
 import com.example.retrofitwithcoroutine_u_test.databinding.ActivityMainBinding
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binder : ActivityMainBinding
@@ -17,12 +21,21 @@ class MainActivity : AppCompatActivity() {
         // get a JSON data with BASE_URL, End point and GSON Converter
         val retrofitService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
 
-        // get data from album
-        val responseLiveData = liveData {
-            val response = retrofitService.getAlbums()
+        // get data with @Path = find data with Album's Id
+        val pathResponse : LiveData<Response<AlbumsItem>> = liveData {
+            val response = retrofitService.getAlbum(3)
             emit(response)
         }
+        pathResponse.observe(this, Observer {
+            val title = it.body()?.title
+            Toast.makeText(this, "title from path: $title", Toast.LENGTH_LONG).show()
+        })
 
+        // get data from album = find data with Album's userId
+        val responseLiveData = liveData {
+            val response = retrofitService.getSortedAlbums(3)
+            emit(response)
+        }
         // observe responseLiveData
         responseLiveData.observe(this, Observer {
             // listIterator: 자바 컬렉션(ArrayList, vector 등)에서 항목을 탐색하는데 쓰이는 커서
@@ -34,7 +47,7 @@ class MainActivity : AppCompatActivity() {
                 while (albumList.hasNext()){
                     // next(): can access, add, remove, modify while list is iterating
                     val albumItem = albumList.next()
-                    val result = " Album id: ${albumItem.title}\n"
+                    val result = " Album userId: ${albumItem.userId} / Album title: ${albumItem.title}\n\n"
                     binder.textView.append(result)
                 }
             }
